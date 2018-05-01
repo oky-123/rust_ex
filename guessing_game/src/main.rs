e44e667eaf8e31a400854681224dea6c74727882
oky-123 <oky123.ia@gmail.com>
2018-05-02 02:57:32 +0900

    Add codes

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,13 @@
 use std::io;
 
 fn main() {
+    println!("Guess the number!");
+    println!("please input your guess.");
+
+    let mut guess = String::new();
+
+    io::std().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
