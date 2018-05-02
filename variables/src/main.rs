f5f1ad8d126f172df857e73a489cecba16a08609
oky-123 <oky123.ia@gmail.com>
2018-05-02 17:46:40 +0900

    Add project

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,19 @@ fn main() {
     println!("The value of x is :{}", x);
     x = 5;
     println!("The value of x is :{}", x);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+    println!("The value of x is :{}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The value of spaces is :{}", spaces);
+
+    let mut spaces = "   ";
+    spaces = "dasa";
+    println!("The value of spaces is :{}", spaces);
 }
