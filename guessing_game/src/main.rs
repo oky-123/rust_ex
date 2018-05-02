72093f0e9f43f6420bd8ae91d44e9ef56a19688c
oky-123 <oky123.ia@gmail.com>
2018-05-02 14:53:40 +0900

    Add error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -19,8 +19,14 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number");
+                continue;
+            }
+        };
+
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
