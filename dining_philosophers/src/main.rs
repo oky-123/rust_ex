6afad0f2bda7fcb8848ce4144c5e1b40a096f617
oky-123 <oky123.ia@gmail.com>
2018-05-03 01:38:59 +0900

    Add strct

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,15 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.toString();
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
