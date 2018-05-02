17155b61f6cae6ae32de3a45066d79aef7991af8
oky-123 <oky123.ia@gmail.com>
2018-05-03 03:01:11 +0900

    Add statements

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,14 +8,33 @@ fn main() {
 
     let x = x + 1;
 
-    let x = x * 2;
     println!("The value of x is :{}", x);
 
-    let spaces = "   ";
-    let spaces = spaces.len();
-    println!("The value of spaces is :{}", spaces);
+    fn add_one(x: i32) -> i32 {
+        x + 1
+    }
 
-    let mut spaces = "   ";
-    spaces = "dasa";
-    println!("The value of spaces is :{}", spaces);
+    let x = add_one(x);
+    println!("The value of x is :{}", x);
+
+    /* 式と文 */
+    // 文1...宣言文
+    // とはかけないので注意
+    let mut y = 1;
+
+    let x = y = 2;
+    // 式ではなく, y = 2 は空のタプル()を返すので
+    // println!("The value of x is :{}", x);
+    // するとエラー
+
+    // 文2...式文
+    // ; は式を文にすることができる
+    // rustでは文の後に文が続く
+
+    // その他は全て式
+
+    /* 関数ポインタ */
+    let f: fn(i32) -> i32 = add_one;
+    let x2 = f(y);
+    println!("The value of y is :{}", x2);
 }
