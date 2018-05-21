0bbbdf7de2585b8260e967da4e8ca0edffadc28b
oky-123 <oky123.ia@gmail.com>
2018-05-22 04:32:22 +0900

    Fix bufreader

--- rsgrep/src/main.rs
+++ rsgrep/src/main.rs
@@ -1,7 +1,7 @@
 // File structをuse
 use std::fs::File;
-// BuffReader struct, BuffRead crateをuse
-use std::io::{BuffReader, BuffRead};
+// BufReader struct, BufRead crateをuse
+use std::io::{BufReader, BufRead};
 // envモジュール全体をuse
 use std::env;
 
@@ -29,5 +29,22 @@ fn main() {
             return;
         }
     };
+
+    // FileをBufReaderに包んで使う
+    let input = BufReader::new(file);
+    // lines()メソッドを使い, イテレータでforを回す
+    for line in input.lines() {
+        // 入力がUTF-8ではないなどの理由で行のパースに失敗することがあるので
+        // `line`もResultに包まれている
+        let line = match line {
+            Ok(line) => line,
+            // エラーなら出力して終了
+            Err(e) => {
+                println!("An error occurred while reading a line {}", e);
+                return;
+            }
+        };
+        println!("{}", line);
+    }
 
 }
