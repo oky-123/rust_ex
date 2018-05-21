8c50b13eda3a95fa87d7c04a4345ab96d3aba6c8
oky-123 <oky123.ia@gmail.com>
2018-05-22 03:24:04 +0900

    Remove target from git staging

--- rust_tutorial/guessing_game/target/debug/deps/guessing_game-b59cbe7e602733dd.d
+++
@@ -1,5 +0,0 @@
-/Users/oky123/rust_ex/guessing_game/target/debug/deps/guessing_game-b59cbe7e602733dd: src/main.rs
-
-/Users/oky123/rust_ex/guessing_game/target/debug/deps/guessing_game-b59cbe7e602733dd.d: src/main.rs
-
-src/main.rs:
--- rust_tutorial/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/oky123/rust_ex/guessing_game/target/debug/guessing_game.dSYM: /Users/oky123/rust_ex/guessing_game/src/main.rs
