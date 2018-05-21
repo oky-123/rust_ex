87b59786e468e788bd72a4dbd75c18949bf4434d
oky-123 <oky123.ia@gmail.com>
2018-05-22 04:10:39 +0900

    open argument as filename

--- rsgrep/src/main.rs
+++ rsgrep/src/main.rs
@@ -5,6 +5,18 @@ use std::io::{BuffReader, BuffRead};
 // envモジュール全体をuse
 use std::env;
 
+fn usage() {
+    println!("rsgrep PATTERN FILENAME");
+}
 fn main() {
-    println!("Hello, world!");
+    let filename = match env::args().nth(2) {
+        // あった場合取り出す
+        Some(filename) => filename,
+        // なければヘルプを表示して終了
+        None => {
+            usage();
+            return;
+        }
+    };
+
 }
