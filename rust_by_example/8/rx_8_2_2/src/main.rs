e13c867eaffe899c253d5346254c5e6b24bfc618
oky-123 <oky123.ia@gmail.com>
2018-05-25 15:58:17 +0900

    Add source code for input parameter

---
+++ rust_by_example/8/rx_8_2_2/src/main.rs
@@ -0,0 +1,34 @@
+fn apply<F>(f: F) where
+    F: FnOnce() {
+        f()
+    }
+
+fn apply_to_3<F>(f: F) -> i32 where
+    F: Fn(i32) -> i32 {
+        f(3)
+    }
+
+fn main() {
+    use std::mem;
+
+    let greeting = "hello";
+    // コピーではなくmoveが起きる型
+    let mut farewell = "good bye".to_owned();
+
+    // 変数を二つ補足
+    let diary = || {
+        println!("I said {}.", greeting);
+
+        // farewell の値を変更するのでこの時点で FnMutが必要
+        farewell.push_str("!!!");
+        println!("Then I screamed {}", farewell);
+        println!("Now, I can sleep.");
+
+        mem::drop(farewell);
+    };
+
+    apply(diary);
+
+    let double = |x| 2 * x;
+    println!("3 doubled: {}", apply_to_3(double));
+}
