7a40b5533752fee83470285b40fec9ec01693bc3
oky-123 <oky123.ia@gmail.com>
2018-06-01 02:05:12 +0900

    Add source code for multi thread

--- rust_by_example/18/thread_supplement/src/main.rs
+++ rust_by_example/18/thread_supplement/src/main.rs
@@ -48,8 +48,43 @@ fn run3() {
     println!("{:?}", rx.recv());
 }
 
+fn run_multi() {
+    let data = vec![1, 2, 3, 4, 5];
+    // for でリファレンス
+    for &x in data.iter() {
+        thread::spawn(move || {
+            // let result = do_something(x);
+            let result = x * 2;
+            let time = rand::random::<u16>() as u64;
+            // thread::sleep(Duration::from_millis(time));
+            println!("{:?}", result);
+        });
+    }
+
+    thread::sleep(Duration::from_millis(500));
+}
+
+fn run_multi2() {
+    let data = vec![1, 2, 3, 4, 5];
+    let (tx, rx) = mpsc::channel();
+
+    for &x in data.iter() {
+        let _tx = tx.clone();
+        // _txはクローン可能で, クロージャの中に所有権を移す
+        thread::spawn(move || {
+            _tx.send(x * 2);
+        });
+    }
+
+    for _ in data.iter() {
+        println!("{:?}", rx.recv());
+    }
+}
+
 fn main() {
     // run1();
     // run2();
-    run3();
+    // run3();
+    run_multi();
+    // run_multi2();
 }
