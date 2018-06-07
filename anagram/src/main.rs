f95cd581a02173477e24104989be525bbb109a9a
oky-123 <oky123.ia@gmail.com>
2018-06-08 03:25:32 +0900

    Implement fn to find anagram

--- anagram/src/main.rs
+++ anagram/src/main.rs
@@ -17,12 +17,35 @@ struct Anagram(HashMap<String, Vec<String>>);
 impl Anagram {
     // トレイト境界`AsRef<Path>`は、ざっくり意訳すると「パス名っぽいもの」を表す
     // `Self`は、`Anagram`へのエイリアス
-    fn new
+    // この構造体は辞書を読み取ってここからanagram用の辞書を作成する
+    fn new<P: AsRef<Path>>(dictfile: P) -> Result<Self, io::Error> {
+        // file ... `Result<Ok(File), Error>`
+        let file = File::open(dictfile)?;
+        let file = io::BufReader::new(file);
+        // preperate anagram
+        let mut anagram = Anagram(HashMap::new());
+        for line in file.lines() {
+            let word = line?;
+            anagram.add_word(word);
+        }
+        Ok(anagram)
+    }
+
+    fn add_word(&mut self, word: String) {
+        let sorted = sorted_string(&word);
+        self.0.entry(sorted).or_insert(Vec::new()).push(word);
+    }
+
+    fn find(&self, word: String) -> Option<&Vec<String>> {
+        let word = sorted_string(&word);
+        self.0.get(&word)
+    }
 }
 
 fn main() {
     let word: String = std::env::args().nth(1).expect("USAGE: word");
     println!("{} is offered.", word);
-    let sorted_word: String = sorted_string(&word);
-    println!("{} is sorted string.", sorted_word);
+    // Unixの場合ここに辞書がある
+    let table = Anagram::new("/usr/share/dict/words").expect("failed to make table");
+    println!("{:?}", table.find(word));
 }
